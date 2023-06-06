9580ec8f9d565d0410b68c409a1fb1f2af773df1
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-06-06 14:45:51 +0700

    show all conversation

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,5 +1,6 @@
 <div>
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+
     <div class="chatlist_header">
         <div class="title">
             Chat
@@ -9,28 +10,32 @@
         </div>
     </div>
 
-    <div class="chatlist_body">
-        <div class="chatlist_item">
-            <div class="chatlist_img_container">
-                <img src="https://picsum.photos/id/237/200/300" alt="">
-            </div>
-            <div class="chatlist_info">
-                <div class="top_row">
-                    <div class="list_username">Zirjy</div>
-                    <span class="date">2d</span>
-                </div>
-                <div class="bottom_row">
-                    <div class="message_body text-truncate">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                        Unde ratione optio, fugit sequi ea temporibus nobis laudantium 
-                        consequatur saepe magni dolores accusamus voluptate, exercitationem 
-                        consectetur at rem facere! Quasi, at.
+    @if (count($conversations) > 0)
+        @foreach ($conversations as $conversation)
+            <div class="chatlist_body">
+                <div class="chatlist_item">
+                    <div class="chatlist_img_container">
+                        <img src="https://picsum.photos/id/{{ $this->getChatUserInstance($conversation, $name = 'id') }}/200/300" alt="">
                     </div>
-                    <div class="unread_count">
-                        100
+                    <div class="chatlist_info">
+                        <div class="top_row">
+                            <div class="list_username">{{ $this->getChatUserInstance($conversation, $name = 'name') }}</div>
+                            <span class="date">{{ $conversation->messages->last()->created_at->shortAbsoluteDiffForHumans() }}</span>
+                        </div>
+                        <div class="bottom_row">
+                            <div class="message_body text-truncate">
+                                {{ $conversation->messages->last()->body }}
+                            </div>
+                            <div class="unread_count">
+                                100
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+        @endforeach
+        @else {
+            You have no conversations
+        }
+    @endif
 </div>
